bc5922837c3fcc1362a089e43704686b9b7cac89
majemase <130973474+majemase@users.noreply.github.com>
2023-12-06 18:19:40 +0100

    Primer commit del integrante 3
    Primera subida del integrante 3 al repositorio, el cual contiene los archivos Config.php, Controller.php, index.php

---
+++ app/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+ class Config
+ {
+     static public $mvc_bd_hostname = "localhost";
+     static public $mvc_bd_nombre   = "alimentos32023";
+     static public $mvc_bd_usuario  = "myarriolab9";
+     static public $mvc_bd_clave    = "mo7JGwAc";
+     static public $mvc_vis_css     = "estilo.css";
+ }
+?>
